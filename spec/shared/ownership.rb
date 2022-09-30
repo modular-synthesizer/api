47166391f6b806d7b6f699d29b05aed833da4ee5
Vincent Courtois <vincent.courtois@coddity.com>
2022-10-01 01:06:22 +0200

    Add mecanism to make standardized tests about ownership
    * Rework the ownership tests
    
    * Add admin exception
    
    * Fix the object creations to not create too much of em in tests
    
    Co-authored-by: Vincent <courtois.vincent@outlook.com>

--- spec/shared/ownership.rb
+++ spec/shared/ownership.rb
@@ -1,19 +1,19 @@
+
 # The shared example to check the errors regarding the ownership of a resource.
 # This route mainly checks that a user cannot access a resource created by
 # another user directly with the given route. in the path, the ":id" part is
 # always replaced with the UUID of the resource created by the given factory.
 RSpec.shared_examples 'ownership' do |verb, path, factory, field=:id|
+
   describe 'Ownership errors' do
     let!(:owner) { create(:account) }
-    let!(:requester) { create(:account) }
-
-    let!(:owner_s) { create(:session, account: owner) }
-    let!(:requester_s) { create(:session, account: requester) }
-
     let!(:resource) { create(factory, account: owner) }
 
+    let!(:requester) { create(:account) }
+    let!(:session) { create(:session, account: requester) }
+
     let!(:payload) do
-      payload = { auth_token: requester_s.token }
+      payload = { auth_token: session.token }
       ['post', 'put'].include?(verb) ? payload.to_json : payload
     end
 
@@ -26,6 +26,30 @@ RSpec.shared_examples 'ownership' do |verb, path, factory, field=:id|
       end
       it 'Returns the correct body' do
         expect(last_response.body).to include_json(
+          key: 'auth_token', message: 'forbidden'
+        )
+      end
+    end
+    # An administrator can do anything on the data for any user.
+    # These great powers come with a shit ton of responsibilities.
+    describe 'Ownership override by admin' do
+      
+      let!(:admin) { create(:account, admin: true) }
+      let!(:session) { create(:session, account: admin) }
+  
+      let!(:payload) do
+        payload = { auth_token: session.token }
+        ['post', 'put'].include?(verb) ? payload.to_json : payload
+      end
+  
+      before do
+        send(verb, path.gsub(':id', resource.send(field).to_s), payload)
+      end
+      it 'Returns anything ranging from 100 to 299 as there is no error' do
+        expect(last_response.status).to be < 299
+      end
+      it 'Does not return an error body' do
+        expect(last_response.body).to_not include_json(
           key: 'auth_token', message: 'forbidden'
         )
       end
