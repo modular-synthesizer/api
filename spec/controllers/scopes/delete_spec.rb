f9fc85da072ea7aef438a454454b221644241643
Vincent <courtois.vincent@outlook.com>
2022-12-22 00:44:49 +0100

    Add the tests for deletion and listing of scopes

--- spec/controllers/scopes/delete_spec.rb
+++ spec/controllers/scopes/delete_spec.rb
@@ -1,3 +1,31 @@
 RSpec.describe 'DELETE /scopes/:id' do
+  def app
+    Modusynth::Controllers::Scopes.new
+  end
 
+  let!(:account) { create(:account, admin: true) }
+  let!(:session) { create(:session, account: account) }
+  let!(:scope) { create(:scope, label: 'Test::Deletion') }
+
+  describe 'Nominal case' do
+    before do
+      delete "/#{scope.id.to_s}", {auth_token: session.token}
+    end
+    it 'Returns a 204 (No Content) status code' do
+      expect(last_response.status).to be 204
+    end
+    it 'Has deleted the scope' do
+      expect(Modusynth::Models::Permissions::Scope.where(label: 'Test::Deletion').count).to be 0
+    end
+  end
+  describe 'Alternative cases' do
+    describe 'When the ID is not found' do
+      before do
+        delete '/unknown', {auth_token: session.token}
+      end
+      it 'Returns a 204 (Not Found) status code' do
+        expect(last_response.status).to be 204
+      end
+    end
+  end
 end
