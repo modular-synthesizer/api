de38a9c18b9ff3afa4d068705788275e56f82d8b
Courtois Vincent <vcourtois@nexity.fr>
2023-07-18 13:53:21 +0200

    Fix some typos in the servics names

--- services/initialization.rb
+++ services/initialization.rb
@@ -15,6 +15,7 @@ module Modusynth
       # If it creates an application, it then updates the secret of the current environment
       # by storing its public and private keys in the corresponding secret.
       def run
+        puts 'Running the initialization service'
         check_and_update_admin!
         check_and_update_application!
       end
@@ -26,10 +27,11 @@ module Modusynth
       end
 
       def check_and_update_admin!
+        puts 'Searching for an administrator account'
         if Modusynth::Models::Account.where(admin: true).to_a.empty?
           puts 'No administrator found. Creating a first account with random password.'
           password = SecureRandom.hex(32)
-          account = Modusynth::Services::Account.create(
+          account = Modusynth::Services::Accounts.create(
             username: 'administrator',
             email: 'contact@synple.app',
             password:, password_confirmation: password,
@@ -41,9 +43,10 @@ module Modusynth
       end
 
       def check_and_update_application!
+        puts 'Searching for an application'
         if Modusynth::Models::OAuth::Application.all.to_a.empty?
           puts 'No application found. Creating a first application'
-          application = Modusynth::Services::OAuth::Application.instance.create(
+          application = Modusynth::Services::OAuth::Applications.instance.create(
             name: 'frontend app',
             account: Modusynth::Models::Account.where(username: 'administrator').first
           )
