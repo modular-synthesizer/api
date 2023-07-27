9262cf50962a3814c6eded9ec67c3aabfa045539
Vincent Courtois <courtois.vincent@outlook.com>
2023-07-27 13:55:24 +0200

    Rubocopification

--- controllers/base.rb
+++ controllers/base.rb
@@ -63,9 +63,7 @@ module Modusynth
         halt status, jbuilder(filename.to_sym, locals:)
       end
 
-      def session
-        @session
-      end
+      attr_reader :session
     end
   end
 end
