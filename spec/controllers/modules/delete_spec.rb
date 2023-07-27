51c0bd18738d917974ed075b5d803d2f93d49cd7
Vincent Courtois <courtois.vincent@outlook.com>
2023-07-27 10:59:45 +0200

    Add tests to check for permissions when deleting a module

--- spec/controllers/modules/delete_spec.rb
+++ spec/controllers/modules/delete_spec.rb
@@ -7,6 +7,9 @@ describe Modusynth::Controllers::Modules do
   let!(:session) { create(:session, account: account) }
   let!(:synthesizer) { Modusynth::Services::Synthesizers.instance.create(account:, name: 'test synth') }
 
+  let!(:guest) { create(:account) }
+  let!(:guest_session) { create(:session, account: guest) }
+
   describe 'DELETE /:id' do
     let(:node) { create(:VCA_module, synthesizer: synthesizer) }
 
@@ -31,6 +34,36 @@ describe Modusynth::Controllers::Modules do
 
         it 'Returns a 204 (No Content) status code' do
           expect(last_response.status).to be 204
+        end
+      end
+      describe 'The module is deleted by another user with the write permission' do
+        let!(:membership) { create(:membership, account: guest, synthesizer:, enum_type: 'write') }
+
+        before do
+          delete "/#{node.id.to_s}", {auth_token: guest_session.token}
+        end
+
+        it 'Returns a 204 (No Content) status code' do
+          expect(last_response.status).to be 204
+        end
+        it 'Has deleted the module' do
+          synthesizer.reload
+          expect(synthesizer.modules.count).to be 0
+        end
+      end
+      describe 'The module is deleted by another user with the read permission' do
+        let!(:membership) { create(:membership, account: guest, synthesizer:, enum_type: 'read') }
+        
+        before do
+          delete "/#{node.id.to_s}", {auth_token: guest_session.token}
+        end
+
+        it 'Returns a 204 (No Content) status code' do
+          expect(last_response.status).to be 204
+        end
+        it 'Has not deleted the module' do
+          synthesizer.reload
+          expect(synthesizer.modules.count).to be 1
         end
       end
     end
