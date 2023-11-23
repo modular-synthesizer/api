df7e7f7849e38e985e723c6e38c04757049d3e08
Vincent <courtois.vincent@outlook.com>
2023-11-23 23:55:48 +0100

    Fix jobs

--- controllers/iut.rb
+++ controllers/iut.rb
@@ -3,21 +3,23 @@
 module Modusynth
   module Controllers
     class Iut < Controllers::Base
-
       post '/' do
         # We first select the collection to make queries on.
         collection = Mongoid.client(:iut)[:books]
-        
-        query = JSON.parse(symbolized_params[:query] || '{}') rescue {}
+        query = begin
+          JSON.parse(symbolized_params[:query] || '{}')
+        rescue StandardError
+          {}
+        end
         field = symbolized_params[:field] || '_id'
         page = symbolized_params[:page] || 0
 
-        case symbolized_params[:action]
-        when 'distinct'
-          results = collection.find.distinct(field).to_a.slice(page * 50, (page + 1) * 50)
-        else
-          results = collection.find(query).limit(50).skip(50 * page)
-        end
+        results = case symbolized_params[:action]
+                  when 'distinct'
+                    collection.find.distinct(field).to_a.slice(page * 50, (page + 1) * 50)
+                  else
+                    collection.find(query).limit(50).skip(50 * page)
+                  end
 
         JSON.generate(results.to_a)
       end
