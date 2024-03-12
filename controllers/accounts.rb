9ebcedc4352ec389d667d62010da42622025bb19
Vincent Courtois <vcourtois@roole.fr>
2024-03-12 13:31:07 +0100

    Mutualize the path to the account view in the dedicated controller

--- controllers/accounts.rb
+++ controllers/accounts.rb
@@ -3,9 +3,12 @@
 module Modusynth
   module Controllers
     class Accounts < Modusynth::Controllers::Base
+
+      ACCOUNT_VIEW = 'accounts/account.json'
+
       api_route 'post', '/', authenticated: false do
         account = service.create(**symbolized_params)
-        render_json 'accounts/account.json', status: 201, account:
+        render_json ACCOUNT_VIEW, status: 201, account:
       end
 
       api_route 'get', '/search' do
@@ -15,22 +18,22 @@ module Modusynth
 
       api_route 'get', '/own' do
         account = service.find_or_fail(id: @session.account.id)
-        render_json 'accounts/account.json', account:
+        render_json ACCOUNT_VIEW, account:
       end
 
       api_route 'get', '/:id', admin: true do
         account = service.find_or_fail(id: params[:id])
-        render_json 'accounts/account.json', account:
+        render_json ACCOUNT_VIEW, account:
       end
 
       api_route 'put', '/:id/groups', admin: true do
         account = service.find_and_update_groups(**symbolized_params)
-        render_json 'accounts/account.json', account:
+        render_json ACCOUNT_VIEW, account:
       end
 
       api_route 'put', '/:id' do
         account = service.find_and_update(**symbolized_params, session: @session)
-        render_json 'accounts/account.json', account:
+        render_json ACCOUNT_VIEW, account:
       end
 
       def service
