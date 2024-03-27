021de70ab3f6f2b08e8a8265db73b0797b196814
Vincent Courtois <vcourtois@roole.fr>
2024-03-27 14:24:32 +0100

    Simplify the result of the tools list to accelerate the loading in the frontend

--- controllers/tools.rb
+++ controllers/tools.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'date'
 
 module Modusynth
   module Controllers
@@ -10,21 +11,22 @@ module Modusynth
       # of fields from the tools to make it as light as possible.
       api_route 'get', '/' do
         criteria = @session.account.admin ? {} : { experimental: false }
-        render_json 'tools/list.json', tools: service.list(**criteria).to_a
+        tools = service.list(**criteria).to_a
+        render_json 'tools/list.json', tools:
       end
 
       api_route 'get', '/:id' do
-        render_json 'tools/_tool.json', tool: service.find_or_fail(id: params[:id])
+        render_json 'tools/details.json', tool: service.find_or_fail(id: params[:id])
       end
 
       api_route 'post', '/', admin: true do
         tool = Modusynth::Services::Tools::Create.instance.create(**symbolized_params)
-        render_json 'tools/_tool.json', status: 201, tool:
+        render_json 'tools/details.json', status: 201, tool:
       end
 
       api_route 'put', '/:id', admin: true do
         tool = Modusynth::Services::Tools::Update.instance.find_and_update(**symbolized_params)
-        render_json 'tools/_tool.json', tool:
+        render_json 'tools/details.json', tool:
       end
 
       api_route 'delete', '/:id', admin: true do
