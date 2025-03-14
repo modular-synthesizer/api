6e6cb23bba76d891d60893da2f27d59153b5a15e
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-03-14 13:44:43 +0100

    Update the parameters in an atomic fashion in a dedicated controller

--- spec/controllers/modules/update_spec.rb
+++ spec/controllers/modules/update_spec.rb
@@ -24,10 +24,6 @@ describe Modusynth::Controllers::Modules do
       it 'Returns a 200 (OK) status code' do
         expect(last_response.status).to be 200
       end
-      it 'Has update the gain value' do
-        node.reload
-        expect(node.parameters.first.value).to be 2.0
-      end
     end
     describe 'Alternative cases' do
       describe 'When the update is done by another user with the write permission' do
@@ -37,7 +33,7 @@ describe Modusynth::Controllers::Modules do
 
         before do
           payload = {
-            parameters: [{value: 2, id: param_id}],
+            rack: 42,
             auth_token: other_session.token
           }
           put "/#{node.id.to_s}", payload.to_json
@@ -45,9 +41,9 @@ describe Modusynth::Controllers::Modules do
         it 'Returns a 200 (OK) status code' do
           expect(last_response.status).to be 200
         end
-        it 'Has updated the value' do
+        it 'Has updated the rack' do
           node.reload
-          expect(node.parameters.first.value).to be 2.0
+          expect(node.rack).to be 42
         end
       end
       describe 'When updating the slot of the module' do
@@ -84,42 +80,6 @@ describe Modusynth::Controllers::Modules do
       end
     end
     describe 'Error cases' do
-      describe 'When the value is below the minimum' do
-        before do
-          payload = {
-            parameters: [{value: -1, id: param_id}],
-            auth_token: session.token
-          }
-          put "/#{node.id.to_s}", payload.to_json
-        end
-
-        it 'Returns a 404 (Not Found) status code' do
-          expect(last_response.status).to be 400
-        end
-        it 'Returns the correct body' do
-          expect(last_response.body).to include_json(
-            key: 'gainparam', message: 'value'
-          )
-        end
-      end
-      describe 'When the value is above the maximum' do
-        before do
-          payload = {
-            parameters: [{value: 101, id: param_id}],
-            auth_token: session.token
-          }
-          put "/#{node.id.to_s}", payload.to_json
-        end
-
-        it 'Returns a 404 (Not Found) status code' do
-          expect(last_response.status).to be 400
-        end
-        it 'Returns the correct body' do
-          expect(last_response.body).to include_json(
-            key: 'gainparam', message: 'value'
-          )
-        end
-      end
       describe 'When the update is made by another user with the read permissions' do
         let!(:other_account) { create(:random_admin) }
         let!(:other_session) { create(:session, account: other_account) }
@@ -127,7 +87,7 @@ describe Modusynth::Controllers::Modules do
 
         before do
           payload = {
-            parameters: [{value: 2, id: param_id}],
+            rack: 42,
             auth_token: other_session.token
           }
           put "/#{node.id.to_s}", payload.to_json
---
+++ spec/controllers/parameters/update_spec.rb
@@ -0,0 +1,102 @@
+describe Modusynth::Controllers::Parameters do
+  def app
+    Modusynth::Controllers::Parameters
+  end
+
+  let!(:account) { create(:random_admin) }
+  let!(:session) { create(:session, account: account) }
+
+  describe 'PUT /:id' do
+    let!(:synth) do
+      Modusynth::Services::Synthesizers.instance.create(account:, name: 'test synth', racks: 2)
+    end
+    let!(:node) { create(:VCA_module, synthesizer: synth) }
+    let!(:param_id) { node.parameters.first.id.to_s }
+
+    describe 'Nominal case' do
+      before do
+        payload = {value: 2, auth_token: session.token }
+        put "/#{param_id}", payload.to_json
+      end
+      it 'Returns a 200 (OK) status code' do
+        expect(last_response.status).to be 200
+      end
+      it 'Has update the gain value' do
+        node.reload
+        expect(node.parameters.first.value).to be 2.0
+      end
+    end
+    describe 'Alternative cases' do
+      describe 'When the update is done by another user with the write permission' do
+        let!(:other_account) { create(:random_admin) }
+        let!(:other_session) { create(:session, account: other_account) }
+        let!(:membership) { create(:membership, account: other_account, synthesizer: synth, enum_type: 'write') }
+
+        before do
+          payload = { value: 2, auth_token: other_session.token }
+          put "/#{param_id}", payload.to_json
+        end
+        it 'Returns a 200 (OK) status code' do
+          expect(last_response.status).to be 200
+        end
+        it 'Has updated the value' do
+          node.reload
+          expect(node.parameters.first.value).to be 2.0
+        end
+      end
+    end
+    describe 'Error cases' do
+      describe 'When the value is below the minimum' do
+        before do
+          payload = { value: -1, auth_token: session.token }
+          put "/#{param_id}", payload.to_json
+        end
+
+        it 'Returns a 404 (Not Found) status code' do
+          expect(last_response.status).to be 400
+        end
+        it 'Returns the correct body' do
+          expect(last_response.body).to include_json(
+            key: 'value', message: 'boundaries'
+          )
+        end
+      end
+      describe 'When the value is above the maximum' do
+        before do
+          payload = { value: 101, auth_token: session.token }
+          put "/#{param_id}", payload.to_json
+        end
+
+        it 'Returns a 404 (Not Found) status code' do
+          expect(last_response.status).to be 400
+        end
+        it 'Returns the correct body' do
+          expect(last_response.body).to include_json(
+            key: 'value', message: 'boundaries'
+          )
+        end
+      end
+      describe 'When the update is made by another user with the read permissions' do
+        let!(:other_account) { create(:random_admin) }
+        let!(:other_session) { create(:session, account: other_account) }
+        let!(:membership) { create(:membership, account: other_account, synthesizer: synth, enum_type: 'read') }
+
+        before do
+          payload = { valuz: 2, auth_token: other_session.token }
+          put "/#{param_id}", payload.to_json
+        end
+        it 'Returns a 403 (Forbidden) status code' do
+          expect(last_response.status).to be 403
+        end
+        it 'Returns the correct body' do
+          expect(last_response.body).to include_json(
+            key: 'auth_token', message: 'forbidden'
+          )
+        end
+      end
+    end
+
+    include_examples 'authentication', 'put', '/:id'
+    include_examples 'scopes', 'put', '/:id'
+  end
+end
