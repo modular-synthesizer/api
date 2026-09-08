da7a4f104d1cae9424f4a2620aaf6d49537e1841
Vincent COURTOIS <vincent.courtois@ecov.fr>
2026-09-08 09:23:05 +0200

    lint

--- models/blueprints/blueprint.rb
+++ models/blueprints/blueprint.rb
@@ -38,7 +38,7 @@ module Modusynth
         embeds_many :inner_links, class_name: '::Modusynth::Models::Blueprints::InnerLink'
 
         # @!attribute [rw] controls
-        #   @return [Array<Modusynth::Models::Blueprints::ControlTemplate>] the list of graphic representation ok controls
+        #   @return [Array<Modusynth::Models::Blueprints::ControlTemplate>]
         has_many :controls, class_name: '::Modusynth::Models::Blueprints::ControlTemplate', inverse_of: :blueprint
         # @!attribute [rw] ports
         #   @return [Array<Modusynth::Models::Blueprints::PortTemplate>] the list of exposed I/O ports for the module.
--- models/concerns/control.rb
+++ models/concerns/control.rb
@@ -11,8 +11,8 @@ module Modusynth
           #   @return [String] the name of the component, on the client-side, used to render this control.
           field :component, type: String
           # @!attribute [rw] payload
-          #   @return [Hash] the attributes passed to the component as props. If an attribute is not defined, the default
-          #     value for this key will be used on the client side.
+          #   @return [Hash] the attributes passed to the component as props. If an attribute is not defined,
+          #     the default value for this key will be used on the client side.
           field :payload, type: Hash, default: {}
 
           validate :component_presence
